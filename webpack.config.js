d51f0c2bd87671e15bf4bb1d5e85a5cf874b5b93
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-22 11:40:06 +0200

    gitignore

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    devServer: {
+        port: 3000,
+        open: true,
+    },
+};
