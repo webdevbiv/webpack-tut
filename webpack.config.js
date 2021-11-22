2225269f030a738402e710f5d3f7b7833d66a229
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-22 15:35:45 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'index.js',
     },
     module: {
